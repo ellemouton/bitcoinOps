39875e28cab362bdf3c8989709072ae6e0cc0a7a
Elle Mouton <elle.mouton@gmail.com>
2020-04-11 16:39:16 +0200

    incorrect curve being used

--- main.go
+++ main.go
@@ -2,62 +2,89 @@ package main
 
 import (
 	"crypto/elliptic"
-	"crypto/sha256"
+	"crypto/rand"
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"math/big"
 
-	"golang.org/x/crypto/ripemd160"
+	"github.com/btcsuite/btcutil/base58"
 )
 
-func main() {
-	// get random 256 bit number (k) -> Private key.
-	// must be a number between 1 and n (the order of the btc ecc)
-	// do this by picking a random numer, then do SHA256 and
-	// check that it is smaller than n. If not, retry.
-	// Use a cryptographically secure pseudorandom number generator (CSPRNG)
-	// and seed from a source with sufficient entropy
-	// Represent the priv key in a number of ways: hex, WIF, WIF-compressed
-	curve := elliptic.P256()
-	privKey := new(big.Int)
-	privKey.Sub(curve.Params().N, big.NewInt(100))
+var curve = elliptic.P256().Params()
 
-	fmt.Println("Private Key:", privKey)
+var compressed = flag.Bool("compressed", true, "Public key in compressed format")
 
-	// Calculate the Public key (K) from the priv key: K = k*G using
-	// the secp256k1 standard. This should result in a (x, y) pair of 32 bytes each
-	// Represent the private key in a number of ways. Compressed and uncompressed
-	KxInt, KyInt := curve.Params().ScalarBaseMult(privKey.Bytes())
-	Kx, Ky := KxInt.Bytes(), KyInt.Bytes()
-
-	ucPubKey := append([]byte{0x04}, Kx...)
-	ucPubKey = append(ucPubKey, Ky...)
-
-	fmt.Println("Pub Key coords:", hex.EncodeToString(Kx), hex.EncodeToString(Ky))
-	fmt.Println(hex.EncodeToString(ucPubKey))
-
-	// Get the bitcoin address. K -> SHA256 -> RIPEMD160 -> A
-	// Encode the address in Base58Check
-	// Do this for both compressed pubkey and uncompressed pubkey
-
-	h256 := sha256.New()
-	h256.Write(ucPubKey)
-
-	rip160 := ripemd160.New()
-	rip160.Write(h256.Sum(nil))
-
-	pubKeyHash := rip160.Sum(nil)
-	fmt.Println("Pubkey hash:", hex.EncodeToString(pubKeyHash))
-	fmt.Println(len(pubKeyHash))
-
-	address := Base58Check(0x00, pubKeyHash)
-
+type PublicKey struct {
+	x          *big.Int
+	y          *big.Int
+	compressed bool
 }
 
-func Base58Check(version byte, payload []byte) []byte {
-	b := append([]byte{version}, payload...)
-	h := sha256.New()
-	h.Write(b)
-	h.Write(h.Sum(nil))
-	return append(b, h.Sum(nil)[:4]...)
+func (p *PublicKey) Hex() []byte {
+	if p.compressed {
+
+	}
+	return append([]byte{0x04}, append(p.x.Bytes(), p.y.Bytes()...)...)
+}
+
+// func (p *PublicKey) Address() {
+//	if p.compressed {
+
+//	}
+
+//	p.x.Bytes()
+//}
+
+type PrivateKey struct {
+	k      *big.Int
+	pubkey *PublicKey
+}
+
+func (p *PrivateKey) Hex() []byte {
+	if p.pubkey.compressed {
+		return append(p.k.Bytes(), 0x01)
+	}
+	return p.k.Bytes()
+}
+
+func (p *PrivateKey) Wif() string {
+	return base58.CheckEncode(p.Hex(), 0x80)
+}
+
+// New creates a new PrivateKey by generating a random number
+func New(compressed bool) *PrivateKey {
+	buf := make([]byte, 32)
+	z := new(big.Int)
+
+	for z.Cmp(big.NewInt(1)) == -1 || z.Cmp(curve.N) == 1 {
+		_, err := rand.Read(buf)
+		if err != nil {
+			return nil
+		}
+		z.SetBytes(buf)
+	}
+
+	KxInt, KyInt := curve.ScalarBaseMult(z.Bytes())
+	pubKey := &PublicKey{
+		x:          KxInt,
+		y:          KyInt,
+		compressed: compressed,
+	}
+
+	return &PrivateKey{
+		k:      z,
+		pubkey: pubKey,
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	pk := New(*compressed)
+	fmt.Println(pk.Wif())
+	fmt.Println(hex.EncodeToString(pk.Hex()))
+	fmt.Println(hex.EncodeToString(pk.pubkey.Hex()))
+	fmt.Println(hex.EncodeToString(pk.pubkey.x.Bytes()))
+	fmt.Println(hex.EncodeToString(pk.pubkey.y.Bytes()))
 }
